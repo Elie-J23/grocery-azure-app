cf5aa952a6768b43bc2c90745598b52444d02ff7
Elie-J23 <eliekwedet@yahoo.com>
2026-04-14 12:00:06 +0200

    test: minimal src startup to isolate Azure 503

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-const express = require("express");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("*", (req, res) => {
-  res.status(200).send("Minimal test app is running");
-});
-
-app.listen(PORT, () => {
-  console.log(`Minimal test app listening on port ${PORT}`);
-});
+require("./src/server");
